240f66013e3e92a75c4044d12664be5a066603a6
Tony Messias <tonyzrp@gmail.com>
2019-05-30 16:42:39 -0300

    Add tests scaffolding

---
+++ tests/Feature/GraphQL/CreateChannelTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\GraphQL;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateChannelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testMustBeAuthenticated()
+    {
+    }
+
+    public function testValidationFails()
+    {
+    }
+
+    public function testCanCreateChannel()
+    {
+    }
+}
